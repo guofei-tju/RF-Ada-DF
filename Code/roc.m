1e0b991c8615832ee73f95b52ac13692b4e45d3f
guofei <39693114+guofei-tju@users.noreply.github.com>
2018-09-02 16:43:10 +0800

    Update roc.m

--- Code/roc.m
+++ Code/roc.m
@@ -9,17 +9,17 @@ FalseNegative = 0;
 max_label = max(test_data_label);
 min_label = min(test_data_label);
 for k=1:l
-    if test_data_label(k)==max_label & predict_label(k)==max_label  %真阳性
+    if test_data_label(k)==max_label & predict_label(k)==max_label  
         TruePositive = TruePositive +1;
     end
-    if test_data_label(k)==min_label & predict_label(k)==min_label %真阴性
+    if test_data_label(k)==min_label & predict_label(k)==min_label 
         TrueNegative = TrueNegative +1;
     end 
-    if test_data_label(k)==min_label & predict_label(k)==max_label  %假阳性
+    if test_data_label(k)==min_label & predict_label(k)==max_label  
         FalsePositive = FalsePositive +1;
     end
 
-    if test_data_label(k)==max_label & predict_label(k)==min_label  %假阴性
+    if test_data_label(k)==max_label & predict_label(k)==min_label  
         FalseNegative = FalseNegative +1;
     end
 end
